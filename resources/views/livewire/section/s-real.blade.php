57213ddf468a226515dbde58056f9db36f2f18f3
kingfac <glodin.kiese@gmail.com>
2023-03-02 14:29:39 +0100

    king social

--- resources/views/livewire/section/s-real.blade.php
+++ resources/views/livewire/section/s-real.blade.php
@@ -3,8 +3,8 @@
     <section class="text-center bg-gray-50 dark:bg-gray-900">
         <div class="px-4 py-8 lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 lg:px-52 xl:px-72">
             <div class="flex flex-col items-center w-full mb-8 text-center lg:mb-16">
-                <h2 class="flex mb-4 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">Nos realisations</h2>
-                <p class="text-gray-500 sm:text-xl dark:text-gray-400">Les realisations faites par Systematik sonts la preuve meme du professionnalisme  </p>
+                <h2 class="flex mb-4 text-4xl font-extrabold tracking-tight text-gray-900 dark:text-white">Ils nous ont fait confiance</h2>
+                <p class="text-gray-500 sm:text-xl dark:text-gray-400">Les realisations faites par Systematik</p>
             </div>
             <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0 ">
                 @foreach($realisations as $realisation)
