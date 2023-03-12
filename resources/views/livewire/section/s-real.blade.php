cb3ff4819492c93d68b8ff3ffce75e4bbd3103e2
kingfac <glodin.kiese@gmail.com>
2023-03-12 19:13:05 +0100

    updated front

--- resources/views/livewire/section/s-real.blade.php
+++ resources/views/livewire/section/s-real.blade.php
@@ -10,8 +10,9 @@
                 @foreach($realisations as $realisation)
 
                     <a class="flex flex-col items-center" href="{{$realisation->url}}" target="_blanc">
-                        <div class="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                            <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg>
+                        <div class="flex items-center justify-center mb-4 rounded-full w-24 h-24">
+                            <img src="{{$realisation->getMedia('realisations')[0]['original_url']}}" class="text-primary-600  dark:text-primary-300">
+                            
                         </div>
                         <h3 class="mb-2 text-xl font-bold dark:text-white">{{$realisation->titre}}</h3>
                         {{-- <p class="text-gray-500 dark:text-gray-400 "></p> --}}
